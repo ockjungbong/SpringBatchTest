24039ef7f17d5e2d169a9f95743573af6ba2f025
devock <ockjungbong@gmail.com>
2022-11-21 23:09:21 +0900

    first commit

---
+++ src/test/java/com/cos/batch/SpringBatchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
