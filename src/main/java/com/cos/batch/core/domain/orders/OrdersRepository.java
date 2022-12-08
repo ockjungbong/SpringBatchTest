5983fd69bd2aa2089b1127df9f83abfb7b77e58f
devock <ockjungbong@gmail.com>
2022-12-09 00:42:23 +0900

    DB 데이터 이관하기(데이터 읽고 쓰기)

---
+++ src/main/java/com/cos/batch/core/domain/orders/OrdersRepository.java
@@ -0,0 +1,6 @@
+package com.cos.batch.core.domain.orders;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdersRepository  extends JpaRepository<Orders, Integer> {
+}
