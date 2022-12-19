29ffad07ad81165473af51bb75100a57245f11ca
devock <ockjungbong@gmail.com>
2022-12-19 23:57:34 +0900

    배치 작업의 기본, 파일 읽기와 쓰기

---
+++ src/main/java/com/cos/batch/job/FileDataReadWrite/PlayerFieldSetMapper.java
@@ -0,0 +1,22 @@
+package com.cos.batch.job.FileDataReadWrite;
+
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+
+import com.cos.batch.job.FileDataReadWrite.dto.Player;
+
+public class PlayerFieldSetMapper implements FieldSetMapper<Player> {
+    public Player mapFieldSet(FieldSet fieldSet) {
+        Player player = new Player();
+
+        player.setID(fieldSet.readString(0));
+        player.setLastName(fieldSet.readString(1));
+        player.setFirstName(fieldSet.readString(2));
+        player.setPosition(fieldSet.readString(3));
+        player.setBirthYear(fieldSet.readInt(4));
+        player.setDebutYear(fieldSet.readInt(5));
+
+        return player;
+    }
+}
+
